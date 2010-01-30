51c6336ebba6e0bd9597bd5f478abf4dd6017a35
Alon Levy <alonlevy1@gmail.com>
2010-01-30 16:16:58 +0200

    forgot help1, help2, eventhandler.py

---
+++ eventhandler.py
@@ -0,0 +1,20 @@
+from keymap import KeyMap
+
+class EventHandler(object):
+
+    # class list of active handlers - they will be called. Any object here can add another
+    # object here. So the menu will add or remove the world as it sees fit, and the world will
+    # remove itself when the game is done
+
+    active_handlers = set()
+
+    def __init__(self):
+        self._keymap = KeyMap() # personal keymap
+
+    def on_key_down(self, key, mod):
+        self._keymap.onkey(key, mod)
+
+    def on_mouse_down(self):
+        print "mouse down - reimplement me %s" % str(self)
+
+
